e97c53310697d69ad86767a5a91f992b2281a544
Marcelo Borges <borgespereira.marcelo@gmail.com>
2021-01-19 11:41:55 +0000

    Add implementation

--- index.js
+++ index.js
@@ -6,22 +6,76 @@ class Node {
 }
 
 class Stack {
+
   constructor(){
     this.top = null;
     this.bottom = null;
     this.length = 0;
   }
+
   peek() {
+    return this.top ? this.top.value : undefined;
   }
-  push(value){
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.length) {
+      this.bottom = newNode;
+      this.top = newNode;
+    } else {
+      const oldTopNode = this.top;
+      this.top = newNode;
+      this.top.next = oldTopNode;
+    }
+
+    this.length++;
   }
-  pop(){
+
+  pop() {
+    if (!this.length) {
+      return undefined;
+    }
+
+    let top;
+    
+    if (this.length === 1) {
+      top = this.top;
+      this.top = null;
+      this.bottom = null;
+      this.length--;
+
+      return top;
+    }
+
+    top = this.top;
+    this.top = this.top.next;
+
+    this.length--;
+
+    return top;
   }
-  //isEmpty
+
+  isEmpty() {
+    return !this.length;
+  }
 }
 
 const myStack = new Stack();
-
+myStack.push("google");
+console.log(myStack.peek());
+myStack.push("Udemy");
+console.log(myStack.peek());
+myStack.push("Discord");
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.push("marcelo");
+console.log(myStack.peek());
 
 
 
